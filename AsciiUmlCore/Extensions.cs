728b0d4b2a0a4f321e462da6f388df2e896130c3
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2017-09-19 21:26:18 +0200

    extract extensions

---
+++ AsciiUmlCore/Extensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AsciiUml
+{
+    public static class Extensions {
+		public static T FirstOrDefault<T>(this IEnumerable<T> collection, Func<T, bool> filter,
+			Action<int> foundActionWithPosition) {
+			int pos = 0;
+
+			var res = collection.FirstOrDefault(
+				x => {
+					bool filterRes = filter(x);
+					if (filterRes) {
+						foundActionWithPosition(pos);
+					}
+					pos++;
+					return filterRes;
+				});
+
+			return res;
+		}
+
+		public static IEnumerable<T> SkipLast<T>(this IEnumerable<T> coll, int skip) {
+			return coll.Reverse().Skip(skip).Reverse();
+		}
+
+		public static void Each<T>(this IEnumerable<T> coll, Action<T> code) {
+			foreach (var c in coll) {
+				code(c);
+			}
+		}
+
+		public static void Each<T>(this IEnumerable<T> coll, Action<T, int> code) {
+			int i = 0;
+			foreach (var c in coll) {
+				code(c, i++);
+			}
+		}
+
+        public static Range<int> MinMax<T>(this IEnumerable<T> coll, Func<T, int> selector)
+        {
+            int min = int.MaxValue, max = int.MinValue;
+            foreach(var c in coll) {
+                var val = selector(c);
+                if (val < min)
+                    min = val;
+                if (val > max)
+                    max = val;
+            }
+            return new Range<int>(min, max);
+        }
+	}
+}
