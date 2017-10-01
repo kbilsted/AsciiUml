7cfc85f3f1e83075dc5e6405e29a5ebd3119e134
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2017-10-01 21:08:38 +0200

    extract

---
+++ AsciiUmlCore/Commands/MoveCursor.cs
@@ -0,0 +1,20 @@
+﻿using AsciiUml.Geo;
+
+namespace AsciiUml.Commands
+{
+    class MoveCursor : ICommand
+    {
+        public readonly Coord delta;
+
+        public MoveCursor(Coord delta)
+        {
+            this.delta = delta;
+        }
+
+        public State Execute(State state)
+        {
+            state.TheCurser = state.TheCurser.Move(delta);
+            return state;
+        }
+    }
+}
--- AsciiUmlCore/Commands/MoveSelectedPaintable.cs
+++ AsciiUmlCore/Commands/MoveSelectedPaintable.cs
@@ -19,20 +19,4 @@ namespace AsciiUml.Commands
 			return state;
 		}
 	}
-
-	class MoveCursor : ICommand
-	{
-		public readonly Coord delta;
-
-		public MoveCursor(Coord delta)
-		{
-			this.delta = delta;
-		}
-
-		public State Execute(State state)
-		{
-			state.TheCurser = state.TheCurser.Move(delta);
-			return state;
-		}
-	}
 }
