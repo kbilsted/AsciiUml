1a19b41dadaba44330a0864fd3f133cf306b4842
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2017-10-19 22:44:25 +0200

    added MultilineInputForm and SinglelineInputForm

---
+++ AsciiUmlCore/UI/MultilineInputForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AsciiConsoleUi;
+
+namespace AsciiUml.UI
+{
+    class MultilineInputForm
+    {
+        private readonly TitledWindow titled;
+        private readonly TextArea textArea;
+        public Action<string> OnSubmit = text => { };
+        public Action OnCancel = () => { };
+
+        public MultilineInputForm(GuiComponent parent, string title, string explanation, Coord position)
+        {
+            titled = new TitledWindow(parent, title) {Position = position};
+
+            new TextLabel(titled, explanation, new Coord(0, 0));
+            var textHeight = 10;
+            textArea = new TextArea(titled, 12, textHeight, new Coord(0, 1)) { OnUserEscape = titled.RemoveMeAndChildren};
+            var ok = new Button(titled, "Ok", () => Submit(), new Coord(2, textArea.RelativePositionToParent.Y + textArea.Dimensions.Height.Pixels + 1))
+            {
+                BackGround = ConsoleColor.DarkGray,
+                Foreground = ConsoleColor.Green
+            };
+            var border = new TextLabel(titled, "", new Coord(0, ok.RelativePositionToParent.Y + 1));
+        }
+  
+        void Submit()
+        {
+            titled.RemoveMeAndChildren();
+            OnSubmit(textArea.Value);
+        }
+
+        public void Focus()
+        {
+            textArea.Focus();
+        }
+    }
+}
---
+++ AsciiUmlCore/UI/SinglelineInputForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using AsciiConsoleUi;
+
+namespace AsciiUml
+{
+    class SinglelineInputForm
+    {
+        private readonly TitledWindow titled;
+        private readonly TextLabel validationErrors;
+        private readonly TextBox selected;
+        public Action<string> OnSubmit = selected => { };
+        public Action OnCancel = () => { };
+
+        public SinglelineInputForm(GuiComponent parent, string title, string explanation, int width, Coord position)
+        {
+            titled = new TitledWindow(parent, title){Position = position};
+
+            new TextLabel(titled, explanation, new Coord(0, 0));
+            selected = new TextBox(titled, width, new Coord(0, 1)) { OnUserEscape = titled.RemoveMeAndChildren, OnUserSubmit = Submit};
+
+            validationErrors = new TextLabel(titled, "", new Coord(0, 2))
+                {
+                    BackGround = ConsoleColor.White,
+                    Foreground = ConsoleColor.Red
+                };
+        }
+  
+        void Submit()
+        {
+            if (string.IsNullOrWhiteSpace(selected.Value))
+            {
+                validationErrors.Text = "Need to fill in a number";
+                return;
+            }
+            titled.RemoveMeAndChildren();
+            OnSubmit(selected.Value);
+        }
+
+        public void Focus() { selected.Focus();}
+    }
+}
